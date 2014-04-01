0a6c91d6fba58a09916451869e135851b60fa51b
citybeams <nicklohr1@gmail.com>
2014-04-01 16:38:21 -0400

    pushed to heroku again

--- config/application.rb
+++ config/application.rb
@@ -20,4 +20,6 @@ module CommandsApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   end
+    config.serve_static_assets = true
+
 end
